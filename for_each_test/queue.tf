8070aab91c93c83b22994ef0345c4fddb9ce8245
Bharath Dasaraju <bharathkumarraju_dasaraju@ntucenterprise.sg>
2020-07-30 10:57:34 +0800

    test count with index

--- for_each_test/queue.tf
+++ for_each_test/queue.tf
@@ -1,6 +1,7 @@
 resource "aws_sqs_queue" "message_queue" {
-  count          = length(var.sqs_names)
+#  count          = length(var.sqs_names)
+  for_each = toset(var.sqs_names)
 
-  name           = format("%s.fifo", element(var.sqs_names, count.index))
+  name           = format("%s.fifo", each.key)
   fifo_queue     = true
 }
