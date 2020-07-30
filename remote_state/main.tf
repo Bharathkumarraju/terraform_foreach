621d91ccf0f9e3847b67acaa589b938ae5faa1eb
Bharath Dasaraju <bharathkumarraju_dasaraju@ntucenterprise.sg>
2020-07-30 10:11:18 +0800

    create remote state

---
+++ remote_state/main.tf
@@ -0,0 +1,43 @@
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+resource "aws_s3_bucket" "bharath_tf_state_store" {
+  bucket = "bharaths-test-terraform-remote-state-aps1"
+
+  # Prevent Accedental Delete of this bucket
+  lifecycle {
+    prevent_destroy = true
+  }
+  # Enables versioning
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "bharaths-terraform-locks" {
+  hash_key = "LockID"
+  name = "bharaths-test-terraform-remote-state-locks-aps1"
+  billing_mode = "PAY_PER_REQUEST"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
+output "bharath_s3_backend_arn" {
+  value = aws_s3_bucket.bharath_tf_state_store.arn
+  description = "The S3 Bucket ARN"
+}
+
+output "bharath_dynamodb_table_name" {
+  value = aws_dynamodb_table.bharaths-terraform-locks.name
+  description = "The Name of the Dynamo DB table"
+}
